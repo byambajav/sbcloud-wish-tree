8bc5bbd154ca7a2cbf69c23c851f3f18a011977b
nghiatt <nghiatt1030@gmail.com>
2016-12-11 08:53:29 +0900

    Improve firework

---
+++ unity/snowflake3d4angles/Assets/Hackathon/Scripts/FireworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FireworkManager : MonoBehaviour {
+
+    public Transform firework1;
+    public Transform firework2;
+
+    // Use this for initialization
+    void Start()
+    {
+        firework1.gameObject.SetActive(false);
+        firework2.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Called");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Play());
+        }
+    }
+
+    IEnumerator Play()
+    {
+        firework1.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        firework2.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        firework1.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        firework2.gameObject.SetActive(false);
+    }
+}
